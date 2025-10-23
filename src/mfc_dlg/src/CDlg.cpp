5b323b914e2d59f6c99d605bc1e7c96f4c71735e
handabao <18862963156@163.com>
2025-10-23 10:08:43 +0800

    MFC创建 Dlg

---
+++ src/mfc_dlg/src/CDlg.cpp
@@ -0,0 +1,30 @@
+﻿// F:\thirty\MyFile\mfc_code\src\mfc_dlg\src\CDlg.cpp: 实现文件
+//
+
+#include "afxdialogex.h"
+#include "CDlg.h"
+#include "resource.h"
+
+// CDlg 对话框
+
+IMPLEMENT_DYNAMIC(CDlg, CDialogEx)
+
+CDlg::CDlg(CWnd* pParent /*=nullptr*/) : CDialogEx(IDD_DLGDEMO, pParent)
+{
+}
+
+CDlg::~CDlg()
+{
+}
+
+void CDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDlg 消息处理程序
