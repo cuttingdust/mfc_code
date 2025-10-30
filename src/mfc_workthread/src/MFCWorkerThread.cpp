031e7761f76cf4f104f8424715610b4d22f7ed05
handabao <18862963156@163.com>
2025-10-30 10:09:34 +0800

    1-17、MFC多线程开发：概论

---
+++ src/mfc_workthread/src/MFCWorkerThread.cpp
@@ -0,0 +1,17 @@
+﻿#include <afxwin.h>
+
+UINT MyFunc(LPVOID)
+{
+    printf("我的线程ID是:%d\n", AfxGetThread()->m_nThreadID);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        AfxBeginThread(MyFunc, NULL);
+    }
+
+    return 0;
+}
