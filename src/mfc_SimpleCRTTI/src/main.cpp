e9315bb5a89abc22d16788db3ebcdc4a4f23b4c3
handabao <18862963156@163.com>
2025-11-12 16:38:57 +0800

    一个简单的RTTI 实现

---
+++ src/mfc_SimpleCRTTI/src/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+class CBoy
+{
+public:
+    const int *GetRuntimeClass()
+    {
+        return &classCBoy;
+    }
+    static const int classCBoy;
+};
+
+const int CBoy::classCBoy = 1;
+
+class CGirl
+{
+public:
+    const int *GetRuntimeClass()
+    {
+        return &classCGirl;
+    }
+    static const int classCGirl;
+};
+
+const int CGirl::classCGirl = 1;
+
+
+int main(int argc, char *argv[])
+{
+    CBoy  boy;
+    CGirl girl;
+    if (boy.GetRuntimeClass() == &CBoy::classCBoy)
+    {
+        printf("这是一个男孩\n");
+    }
+
+    if (girl.GetRuntimeClass() == &CGirl::classCGirl)
+    {
+        printf("这是一个女孩\n");
+    }
+
+    return 0;
+}
